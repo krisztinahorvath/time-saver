4cfea9c6f2392beb7fc4559bfd4d54308424e8a2
CosminAchitei <cosadi2002@gmail.com>
2026-06-14 08:20:54 +0300

    Added validator for job posts since I forgot it beforehand

---
+++ TimeSaverAPI/Validators/CreateJobPostDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using TimeSaverAPI.DTOs;
+
+namespace TimeSaverAPI.Validators
+{
+    public class CreateJobPostDtoValidator : AbstractValidator<CreateJobPostDto>
+    {
+        public CreateJobPostDtoValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(100).WithMessage("Title cannot exceed 100 characters.");
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Description is required.")
+                .MaximumLength(1000).WithMessage("Description cannot exceed 1000 characters.");
+
+            RuleFor(x => x.Budget)
+                .GreaterThan(0).WithMessage("Budget must be greater than 0.");
+        }
+    }
+}
